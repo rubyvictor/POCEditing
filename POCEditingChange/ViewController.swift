e375c6c0f4e734e0ebedeac8cefab4273f90b7d6
Victor Lee <myleevictor@gmail.com>
2018-10-29 13:16:57 +0800

    Initial Commit

---
+++ POCEditingChange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POCEditingChange
+//
+//  Created by Victor Lee on 29/10/18.
+//  Copyright © 2018 VictorLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
