7dbd615c05ac372832a3422037fec301373919f9
Victor Lee <myleevictor@gmail.com>
2018-10-30 15:42:37 +0800

    space added after 4 numbers

--- POCEditingChange/ViewController.swift
+++ POCEditingChange/ViewController.swift
@@ -8,13 +8,104 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    let textField: UITextField = {
+       let tf = UITextField()
+        tf.textAlignment = .center
+        tf.placeholder = "credit card number"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = .gray
+        tf.borderStyle = .line
+        tf.addTarget(self, action: #selector(textFieldChanged(textField:)), for: .editingChanged)
+        return tf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        
+        textField.delegate = self
+
+        view.addSubview(textField)
+        
+        setupTextField()
+        
+        
     }
 
+    @objc private func textFieldChanged (textField: UITextField) {
+        print("entering credit card number")
+        guard let count = textField.text?.count else { return }
+        if count == 4 || count == 9 || count == 14 {
+            guard var text = textField.text else { return }
+            text = text + " "
+            textField.text = text
+        }
+        print(count)
+    }
+    
+    private func setupTextField(){
+        textField.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 10).isActive = true
+        textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        textField.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if let text: NSString = (textField.text ?? "") as? NSString {
+            let changedText = text.replacingCharacters(in: range, with: string)
+//            deleteBackwards(on: changedText)
+            checkMaxLength(textField, maxLength: 16, string: string)
+            return true
+        }
+    }
+        
+    func deleteBackwards(on string: String) {
+        if string.isBackspace {
+            textField.deleteBackward()
+        }
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        deleteBackwards(on: "")
+        return true
+    }
+    
+    
+//        guard let text = textField.text else { return true }
+//        let count = text.count + string.count - range.length
+//
+//        return count <= 19
+//    }
+    
+    private func checkMaxLength(_ textField: UITextField, maxLength: Int, string: String) {
+        guard let count = textField.text?.count else { return }
+        // Need to check for empty space otherwise cannot delete backwards from the space
+        if count == 19 || string.isBackspace {
+            
+            let finalText = string.replacingOccurrences(of: " ", with: "")
+            textField.text = finalText
+            textField.deleteBackward()
+        }
+    
+//        if string.isBackspace {
+//            if string.count >= maxLength {
+//                let newString = string.trimmingCharacters(in: NSCharacterSet.whitespaces)
+//                textField.text = newString
+//                textField.deleteBackward()
+//            }
+//        }
+    }
 
 }
+
+extension String {
+    var isBackspace: Bool {
+        let char = self.cString(using: String.Encoding.utf8)!
+        return strcmp(char, "\\b") == -92
+    }
+}
+
 
